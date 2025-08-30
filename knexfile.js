86d22ccfc63ee727edf15ea999fd38e8d2b27fcf
Lucas <diaslucas8822@gmail.com>
2025-08-30 15:40:34 -0300

    first commit

--- knexfile.js
+++ knexfile.js
@@ -13,8 +13,8 @@ module.exports = {
             host: "127.0.0.1",
             port: 5432,
             user: process.env.POSTGRES_USER,
-            password: process.env.POSTGRES_PASSWORD,
-            database: process.env.POSTGRES_DB,
+            password: "83782813",
+            database: "postgres",
         },
         migrations: {
             directory: "./db/migrations",
